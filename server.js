68785627f905db17d94d599849cd29f59c647f25
lake1116 <mipeop0725@gmail.com>
2025-03-25 21:08:14 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(){
+    console.log('listening on 8080')
+});
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html');
+});
+
+
+
+
+
+
+
